27b238f5231dfc8e5898a4cc58b351395b802c99
Sowmith Manepalli <sowmithmanepalli@Sowmiths-MacBook-Pro.local>
2015-12-20 03:00:44 +0530

    Login

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -8,22 +8,60 @@
  */
 package com.parse.starter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.parse.GetCallback;
 import com.parse.ParseAnalytics;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.text.ParseException;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    final Context context = this;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
+
+      final EditText email = (EditText)findViewById(R.id.editText);
+      Button login = (Button)findViewById(R.id.button);
+
+      login.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              ParseQuery<ParseObject> query = ParseQuery.getQuery("User");
+              query.whereEqualTo("email", email.getText().toString());
+              query.getFirstInBackground(new GetCallback<ParseObject>() {
+                  @Override
+                  public void done(ParseObject object, com.parse.ParseException e) {
+                      if (object == null) {
+                          //new userCode
+                          ParseObject gameScore = new ParseObject("User");
+                          gameScore.put("email", email.getText().toString());
+                          gameScore.saveInBackground();
+                          Log.d("objectCreated",":)");
+                      }
+                  }
+              });
+
+              Intent nextScreen = new Intent(context,mainScreen.class);
+              startActivity(nextScreen);
+          }
+      });
   }
 
   @Override
